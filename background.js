15f1125e3a98c35745bca575be1672df10ead7bb
Matthew Horwood <matt.horwood@mayden.co.uk>
2017-04-18 16:10:57 +0100

    start to make this cross browser compatible

--- background.js
+++ background.js
@@ -1,7 +1,31 @@
 /**
+ * find out what the browser is
+ **/
+var chrome_browser;
+navigator.browserInfo= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+if(navigator.browserInfo.indexOf('Firefox') != -1){
+    chrome_browser = browser;
+}else{
+    chrome_browser = chrome;
+}
+/**
  * Detect Orbit's URL everytimes a tab is reloaded or openned
  */
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome_browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === "complete") {
         checkForValidUrl(tab);
     }
@@ -10,7 +34,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 /**
  * Manage local storage between extension & content script
  */
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+chrome_browser.extension.onMessage.addListener(function(request, sender, sendResponse) {
     // local storage request
     if (request.storage) {
         if (typeof request.value !== 'undefined') {
@@ -30,12 +54,12 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
  */
 function checkForValidUrl(tab) {
     if ((tab.url.indexOf('https://beta-crm.mayden.co.uk/tasks/')) == 0 || (tab.url.indexOf('https://crm.mayden.co.uk/tasks/') == 0)) {
-        chrome.pageAction.show(tab.id);
+        chrome_browser.pageAction.show(tab.id);
         
         //console.log(tab.url);
         
         // Call content-script initialize function
-        chrome.tabs.sendMessage(
+        chrome_browser.tabs.sendMessage(
             //Selected tab id
             tab.id,
             //Params inside a object data
--- new.js
+++ new.js
@@ -1,3 +1,28 @@
+/**
+ * find out what the browser is
+ **/
+var chrome_browser;
+navigator.browserInfo= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+if(navigator.browserInfo.indexOf('Firefox') != -1){
+    chrome_browser = browser;
+}else{
+    chrome_browser = chrome;
+}
+
 const allTasks = new Set()
 const seperator = '----------------------'
 var cardData;
@@ -26,7 +51,7 @@ const doStuff = function (task) {
     addCardButton.innerHTML = "\n\
 <span id=\"gttButton\" class=\"T-I J-J5-Ji ar7 nf T-I-ax7 L3 label\" data-tooltip=\"Add this card to Trello\">\n\
     <div aria-haspopup=\"true\" role=\"button\" class=\"J-J5-Ji W6eDmd L3 J-J5-Ji Bq L3\" tabindex=\"0\" style=\"display: inline-block;\">\n\
-        <!-- <img class=\"f tk3N6e-I-J3\" src=\"" + chrome.extension.getURL('images/icon-13.jpg') + "\">\n -->\
+        <!-- <img class=\"f tk3N6e-I-J3\" src=\"" + chrome_browser.extension.getURL('images/icon-13.jpg') + "\">\n -->\
         <span class=\"button-text\">Add card</span>\n\
     </div>\n\
 </span>";
